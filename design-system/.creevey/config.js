674cdd12ab9586cfee6f32fc3899e3b96b43d847
Bridge Tools <robot@bridgetools.io>
2021-05-04 16:51:24 +0300

    Less strict Creevey config

--- design-system/.creevey/config.js
+++ design-system/.creevey/config.js
@@ -3,4 +3,5 @@ const path = require('path');
 module.exports = {
   screenDir: path.join(__dirname, '../tests/referenceImages'),
   reportDir: path.join(__dirname, '../tests/report'),
+  diffOptions: { threshold: 0.2 },
 }
