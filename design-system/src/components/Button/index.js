44f42b9ce2508aa689e57cc788d5ebc52e0f2b6d
Irina Illustrova <illustrova@gmail.com>
2021-05-24 10:50:36 +0300

    fix component styling according coded repo

--- design-system/src/components/Button/index.js
+++ design-system/src/components/Button/index.js
@@ -7,6 +7,16 @@ import React from "react";
 import styled, { css } from "styled-components";
 import { colors } from "../../tokens";
 import { bool, string } from "prop-types";
+import { StyledIcon } from "../Icon";
+
+const Button = ({ as, label, children, ...props }) => {
+  const isIcon = children?.type?.displayName === "Icon";
+  return (
+    <StyledButton as={as} isIcon={isIcon} {...props}>
+      {label ? label : children}
+    </StyledButton>
+  );
+};
 
 const StyledButton = styled.button`
   display: inline-flex;
@@ -15,36 +25,51 @@ const StyledButton = styled.button`
   cursor: pointer;
   outline: none;
   border: 1px solid;
-  border-color: ${(p) => (p.isOutline ? colors.onSurface100 : "transparent")};
+  border-color: transparent;
   box-sizing: border-box;
   line-height: 24px;
   position: relative;
   user-select: none;
   text-decoration: none;
   border-radius: 1rem;
-  padding: ${(p) => (p.label ? "16px" : "12px")};
-  background-color: ${(p) => (p.isOutline ? "transparent" : p.color)};
-  opacity: ${(p) => (p.disabled ? 0.6 : 1)};
-  color: ${(p) =>
-    p.isOutline ? colors.onBackground500 : colors.background500};
+  padding: ${({ isIcon }) => (isIcon ? "12px" : "16px")};
+  background-color: ${({ color }) => color};
+  opacity: ${({ disabled }) => (disabled ? 0.6 : 1)};
+  color: ${colors.background500};
   font-size: 14px;
   font-weight: 700;
   width: ${(p) => (p.isStretched ? "100%" : null)};
-`;
+  ${({ isOutline }) =>
+    isOutline &&
+    `
+     border-color: ${colors.onSurface100}; 
+     background-color: transparent;
+     color: ${colors.onBackground500};
+  `};
+  ${({ isBorderless }) =>
+    isBorderless &&
+    `
+     background-color: transparent;
+     color: ${colors.primary500};
+     font-size: 18px;
+     line-height: 28px;
+     font-weight: 500;
+     letter-spacing: -0.4px;
+     padding: 0;
+  `};
 
-const Button = ({ href, label, children, to, ...props }) => {
-  return (
-    <StyledButton as={href && "a"} href={href} {...props}>
-      {label ? label : children}
-    </StyledButton>
-  );
-};
+  &:has(span) {
+    padding: 12px;
+    background: blue;
+  }
+`;
 
 Button.propTypes = {
   label: string,
   disabled: bool,
   isStretched: bool,
   isOutline: bool,
+  isBorderless: bool,
   color: string,
 };
 
